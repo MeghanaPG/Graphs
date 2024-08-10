348f15dd13d1589416519fc3f12a90d6997c2204
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-08-10 16:14:44 -0400

    Time: 89 ms (64.05%), Space: 18.4 MB (30.21%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,15 +1,16 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        # Time Complexity: O(V+E)
+        # Time: O(V+E)
+        # Union Find 
+        # 
+
         par = [i for i in range(n)]
         rank = [1] * n
 
-        # this function is to find the parent 
         def find(n1):
             res = n1
-            # while res is != parent of itself 
-            while res !=par[res]:
-                # we will link the parent of res to its grand parent 
+
+            while res != par[res]:
                 par[res] = par[par[res]]
                 res = par[res]
             return res 
@@ -17,20 +18,20 @@ class Solution:
         def union(n1,n2):
             p1, p2 = find(n1), find(n2)
 
+            # this means we are not performing the union 
             if p1 == p2:
-                return 0
+                return 0 
             
             if rank[p2] > rank[p1]:
-                par[p1] = p2
-                rank[p2] += rank[p1]
+                par[p1] = p2 
+                rank[p2] += 1 
             else:
                 par[p2] = p1
-                rank[p1] += rank[p2] 
-            # this indicates that we performed a successful union 
+                rank[p2] += 1 
             return 1 
         
         res = n 
-        for n1,n2 in edges:
-            # every time we perform an union the number of connected components will decrease 
+        for n1, n2 in edges:
             res -= union(n1,n2)
-        return res
+        return res 
+
