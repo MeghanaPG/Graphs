ed9b0463e943d133b8c421010c3ab498e6dbdb2c
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-08-10 14:54:11 -0400

    Time: 93 ms (37.96%), Space: 18.3 MB (73.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,32 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Graph, DFS 
         # Time Complexity: O(n+p)
-        # for each crs we will have some prerequisites 
-        if len(prerequisites) == 1:
-            return True 
-
         preMap = {i:[] for i in range(numCourses)}
+
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
 
-        # visitset: all courses along dfs 
-        visitSet = set()
+        visit = set()
+
         def dfs(crs):
-            if crs in visitSet:
+            # indicates a loop
+            if crs in visit:
                 return False 
+            # no pre 
             if preMap[crs] == []:
                 return True 
             
-            visitSet.add(crs)
+            visit.add(crs)
             for pre in preMap[crs]:
                 if not dfs(pre):
                     return False 
-            visitSet.remove(crs)
+            visit.remove(crs)
+            # bcz it will be true 
             preMap[crs] = []
-            return True 
-        
+            return True
+
+        # manually check each course as they may not be connected  
         for crs in range(numCourses):
             if not dfs(crs):
-                return False
+                return False 
         return True 
