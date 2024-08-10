660a00dc5b68727a3541e3eaa23219c8c9f1a644
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-08-10 15:46:55 -0400

    Time: 85 ms (66.59%), Space: 18.7 MB (15.59%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,34 +1,30 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        # Time Complexity: O(E+V)
-        if not n:
-            return True
-        
-        # create the adjacency list 
-        adj = {i:[] for i in range(n)}
+        # Time: O(V+E), M: O(V+E)
+        # set 
+        # dfs
 
+        if n == 0:
+            return True
+
+        adj = {i:[] for i in range(n)}
         for n1, n2 in edges:
-            # the edges are undirected and hence we append each as their respective adj items 
             adj[n1].append(n2)
             adj[n2].append(n1)
 
         visit = set()
         def dfs(i, prev):
-            # this will detect the cycle 
             if i in visit:
-                return False 
+                return False
             
             visit.add(i)
-            # now we have to go through every single neighbour of i 
+            # go through neighbors of i
             for j in adj[i]:
-                # this means if the prev node is already visited then we just continue
                 if j == prev:
                     continue 
-                # this means we have detected the cycle 
                 if not dfs(j,i):
                     return False 
             return True 
         
-        # here we are sending -1 as prev of 0 as it doesn't have any prev 
-        # below the second condition makes sure that the graph is connected or it is a tree
-        return dfs(0,-1) and n == len(visit)
+        return dfs(0, -1) and n == len(visit)
+
