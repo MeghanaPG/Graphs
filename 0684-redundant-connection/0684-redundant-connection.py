7c6bde3f3258003b598adbb4c258e9c52d1ed357
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-08-10 16:56:22 -0400

    Time: 52 ms (75.54%), Space: 17.1 MB (27.16%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,33 +1,35 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        parent = [i for i in range(len(edges)+1)]
-        rank = [1] * (len(edges) + 1)
+        # Time: O(n)
+        # Union Find
 
-        # we will first write the find part of Unionfind algorithm 
+        par = [i for i in range(len(edges)+1)]
+        rank = [1] * (len(edges) + 1) 
+
         def find(n):
-            p = parent[n]
-            while p != parent[p]:
-                parent[p] = parent[parent[p]]
-                p = parent[p]
+            p = par[n]
+
+            while p != par[p]:
+                par[p] = par[par[p]]
+                p = par[p]
             return p 
         
-        # in the union part, we have to find it's root parent 
-        #  return false if they have the same parent, because with that we cannot complete union operation 
-        def union(n1,n2):
+        # return False if can't complete 
+        def union(n1, n2):
             p1, p2 = find(n1), find(n2)
-            
+
             if p1 == p2:
                 return False 
-            
+
             if rank[p1] > rank[p2]:
-                # p1 is gonna be the parent of p2 
-                parent[p2] = p1
+                par[p2] = p1
                 rank[p1] += rank[p2]
             else:
-                parent[p1] = p2
+                par[p1] = p2
                 rank[p2] += rank[p1]
             return True 
-        
+
         for n1, n2 in edges:
             if not union(n1,n2):
-                return [n1,n2]
+                return [n1,n2]            
+
