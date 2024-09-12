4d96061d7f4d5aca46c9b5febc7f18ee0cb70b1b
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-12 10:48:10 -0400

    Time: 312 ms (13.05%), Space: 24.3 MB (23.64%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,7 +3,7 @@ class Solution:
         islands = 0 
 
         if not grid:
-            return 0 
+            return 
         
         rows, cols = len(grid), len(grid[0])
         visit = set()
@@ -15,22 +15,19 @@ class Solution:
 
             while q:
                 row, col = q.popleft()
-                directions = [(-1,0), (1,0), (0,-1), (0,1)]
+                directions = [(-1,0), (1,0), (0,1), (0,-1)]
 
                 for dr, dc in directions:
-                    r, c = row+dr, col + dc
-                    if(r in range(rows) and c in range(cols) and
-                    (r,c) not in visit and grid[r][c] == "1"):
+                    r, c = row + dr, col + dc 
+                    if (r in range(rows) and c in range(cols) and (r,c) not in visit and grid[r][c] == "1"):
                         q.append((r,c))
                         visit.add((r,c))
-        
+            
+
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r,c) not in visit:
                     bfs(r,c)
                     islands += 1
-        
         return islands
-
-            
 
